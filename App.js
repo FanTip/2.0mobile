f289d7a310c43e36c671a07b01198a6281ecb468
w0nght <wwongheitung@gmail.com>
2018-11-15 14:43:30 +1100

    App update

--- App.js
+++ App.js
@@ -1,27 +1,123 @@
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
+// Import Constructor
+import React, { Component } from 'react';
+import { 
+	StyleSheet, 
+	View, 
+	Text,
+	Image,
+	TouchableOpacity
+} from 'react-native';
+import { 
+	createSwitchNavigator, 
+	createStackNavigator, 
+	createDrawerNavigator,
+	createBottomTabNavigator
+} from 'react-navigation'
+import Icon from 'react-native-vector-icons/Ionicons'
 
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>Hello World!</Text>
-//         <Text>trying out the live update !!!! ~~~ ~!!!!!</Text>
-//       </View>
-//     );
-//   }
-// }
+import SplashScreen 		from './screens/SplashScreen';
+import LoginScreen 			from './screens/LoginScreen';
+import RegisterScreen 		from './screens/RegisterScreen';
+import AuthLoadingScreen	from './screens/AuthLoadingScreen';
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+import HomeScreen			from './screens/HomeScreen';
+import SettingsScreen		from './screens/SettingsScreen';
+import NotificationsScreen	from './screens/NotificationsScreen';
+import MessagesScreen		from './screens/MessagesScreen';
 
+import FanTipHistory		from './screens/FanTipHistory';
+import About				from './screens/About';
 
-import App from './index.android.js';
+const AuthStackNavigator = createStackNavigator({
+	SplashScreen: SplashScreen,
+	LoginScreen: LoginScreen,
+	RegisterScreen: RegisterScreen
+})
 
-export default App;
+const AppTabNavigator = createBottomTabNavigator({
+	NotificationsScreen: {
+		screen: NotificationsScreen,
+		title: 'Notifications'
+	},
+	MessagesScreen: {
+		screen: MessagesScreen,
+	},
+	HomeScreen: {
+		screen: HomeScreen,
+	}
+	// ,
+	// Menu: {
+	// 	screen: AppDrawerNavigator
+	// }
+})
+
+const AppStackNavigator = createStackNavigator({
+	AppTabNavigator: {
+		screen: AppTabNavigator,
+		navigationOptions: ({ navigation }) => ({
+			headerTitle: (
+		  		<View style={{flex:1, flexDirection:'row', justifyContent:'center'}}>
+        			<Image resizeMode="cover" 
+         			  source={require('./app/img/Fan_Tipper_Logo_Green.png')}
+         			  style={{
+         			  	width: 120, 
+         			  	height: 50, 
+         			  	resizeMode:'contain',
+         			  	alignItems: 'center' 
+         			  }}
+      			 	 />
+  				</View>
+				),
+			headerLeft: (
+				<TouchableOpacity onPress={() => navigation.toggleDrawer()}>
+					<View style={{ paddingHorizontal: 10 }}>
+						<Icon name='md-menu' size={24} />
+					</View>
+				</TouchableOpacity>
+			)
+		})
+	}
+})
+
+const AppDrawerNavigator = createDrawerNavigator({
+	Home: AppStackNavigator,
+	FanTipHistory: {
+		screen: FanTipHistory,
+		navigationOptions: {
+			drawerLabel: 'FanTip History',
+			drawerIcon: ({ tintColor }) => <Icon name="ios-cog" size={24} />,
+		}
+	},
+	About: {
+		screen: About,
+		navigationOptions: {
+			drawerLabel: 'About FanTipper',
+			drawerIcon: ({ tintColor }) => <Icon name="ios-cog" size={24} />,
+		}
+	},
+	SettingsScreen: {
+		screen: SettingsScreen,
+		navigationOptions: {
+			drawerLabel: 'Account Settings',
+			drawerIcon: ({ tintColor }) => <Icon name="ios-cog" size={24} />,
+		}
+	}
+})
+
+export default createSwitchNavigator({
+	AuthLoading: AuthLoadingScreen,
+	Auth: AuthStackNavigator,
+	App: AppDrawerNavigator
+})
+
+const styles = StyleSheet.create ({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
+});
+
+// import App from './index.android.js';
+
+// export default App;
--- index.android.js
+++ index.android.js
@@ -1,35 +1,66 @@
+// Import Constructor
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, View } from 'react-native';
-import { createStackNavigator } from 'react-navigation'
-import { createBottomTabNavigator } from 'react-navigation'
 
+import { 
+	createSwitchNavigator, 
+	createStackNavigator 
+} from 'react-navigation'
 
-import SplashScreen from './screens/SplashScreen'
-import LoginScreen from './screens/LoginScreen'
-import HomeScreen from './screens/HomeScreen'
+// Import Screens
+import SplashScreen 		from './screens/SplashScreen';
+import LoginScreen 			from './screens/LoginScreen';
+import RegisterScreen 		from './screens/RegisterScreen';
+import AuthLoadingScreen	from './screens/AuthLoadingScreen';
+//import HomeScreen from './screens/HomeScreen';
+//import BottomTabs from './app/components/Navigators';
+//import AppDrawerNavigator from './app/components/MenuDrawer';
 
-//export default class ProfilePage extends Component {
-export default class App extends React.Component {
-	render() {
-		return (
-			<AppStackNavigator />
-		);
-	}
-}
-
-const AppStackNavigator = createStackNavigator({
+// export default class App extends React.Component {
+// 	render() {
+// 		return (
+// 			 //<BottomTabs />
+// 			//<Tabs />
+// 			<AppStackNavigator />
+// 			//<AppDrawerNavigator />
+// 		);
+// 	}
+// }
+const AuthStackNavigator = createStackNavigator({
 	Splash: SplashScreen,
 	Login: LoginScreen,
-	Home: HomeScreen
+	Register: RegisterScreen
 })
+
+export default createSwitchNavigator({
+	AuthLoading: AuthLoadingScreen,
+	Auth: AuthStackNavigator,
+})
+
+
+
+
+// const AppStackNavigator = createStackNavigator({
+// 	Splash: { screen: SplashScreen },	//welcome screen
+// 	Login: { screen: LoginScreen },
+// 	Register: { screen: RegisterScreen }
+// }
+// ,
+ // {
+	// navigationOptions: {
+	// 	gesturesEnabled: false
+	// }
+// }
 
 
 const styles = StyleSheet.create ({
 	container: {
 		flex: 1,
 		backgroundColor: '#000',
+		alignItems: 'center',
+		justifyContent: 'center'
 	},
 
 });
 
-//AppRegistry.registerComponent('ProfilePage', () => ProfilePage);
+//AppRegistry.registerComponent('ProfilePage', () => BottomTabs);
